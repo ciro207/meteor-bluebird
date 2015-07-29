1bef6ba6cd9701c3f459184a4f12168516d5167c
jrudio <justinjrudio@gmail.com>
2015-07-29 02:03:14 -0700

    Added git repo

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Bluebird v2.9.34 - Bluebird is a fully featured promise library with focus on innovative features and performance',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/jrudio/meteor-bluebird',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -12,7 +12,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.addFiles('bluebird.js');
+  api.addFiles('bluebird.js'); // Available on both server and client
 });
 
 Package.onTest(function(api) {
