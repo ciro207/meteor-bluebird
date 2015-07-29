5ede2f43e66265bbe9958cfb41782b4806806362
jrudio <justinjrudio@gmail.com>
2015-07-29 02:15:59 -0700

    Updated summary to comply with atmosphere's summary criteria

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'jrudio:bluebird',
   version: '2.9.34',
   // Brief, one-line summary of the package.
-  summary: 'Bluebird v2.9.34 - Bluebird is a fully featured promise library with focus on innovative features and performance',
+  summary: 'Bluebird v2.9.34',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/jrudio/meteor-bluebird',
   // By default, Meteor will default to using README.md for documentation.
